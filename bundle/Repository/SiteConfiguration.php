3c8f8eb553e0c1c799717b0938a4bea757bc8770
Anis KOSSENTINI <anis.kossentini-ext@almaviacx.com>
2023-02-14 16:58:59 +0100

    fix use / namesapces

--- bundle/Repository/SiteConfiguration.php
+++ bundle/Repository/SiteConfiguration.php
@@ -13,11 +13,17 @@ declare(strict_types=1);
 
 namespace Novactive\Bundle\eZSiteAccessFactoryBundle\Repository;
 
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\QueryBuilder;
+use Doctrine\Persistence\ManagerRegistry;
 use Novactive\Bundle\eZSiteAccessFactoryBundle\Entity\SiteConfiguration as SiteConfigurationEntity;
 
-class SiteConfiguration extends EntityRepository
+class SiteConfiguration extends ServiceEntityRepository
 {
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, SiteConfigurationEntity::class);
+    }
     protected function getAlias(): string
     {
         return 'sc';
